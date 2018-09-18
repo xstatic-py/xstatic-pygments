08af296b337a677a54ba6612b022fc4beb35192d
Thomas Waldmann <tw@waldmann-edv.de>
2018-09-18 20:39:04 +0200

    remove styles that threw error msgs when trying to use them

--- genstyles.sh
+++ genstyles.sh
@@ -1,6 +1,6 @@
 for S in abap algol_nu algol arduino autumn borland bw colorful \
          default emacs friendly fruity igor lovelace manni monokai \
-         murphy native paraiso_dark paraiso_light pastie perldoc \
+         murphy native pastie perldoc \
          rainbow_dash rrt sas stata tango trac vim vs xcode ; do
     pygmentize -f html -S $S > xstatic/pkg/pygments/data/css/$S.css
 done
