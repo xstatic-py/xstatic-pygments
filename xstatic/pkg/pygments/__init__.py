21d4fec9277695120609eca150ad04b062c496ed
Thomas Waldmann <tw@waldmann-edv.de>
2020-11-08 00:49:19 +0100

    update xstatic metadata

--- xstatic/pkg/pygments/__init__.py
+++ xstatic/pkg/pygments/__init__.py
@@ -11,7 +11,7 @@ NAME = __name__.split('.')[-1] # package name (e.g. 'foo' or 'foo_bar')
                                # please use a all-lowercase valid python
                                # package name
 
-VERSION = '2.2.0' # version of the packaged files, please use the upstream
+VERSION = '2.7.2' # version of the packaged files, please use the upstream
                   # version number
 BUILD = '1' # our package build number, so we can release new builds
              # with fixes for xstatic stuff.
@@ -28,7 +28,7 @@ MAINTAINER = 'Thomas Waldmann'
 MAINTAINER_EMAIL = 'tw@waldmann-edv.de'
 
 # this refers to the project homepage of the stuff we packaged:
-HOMEPAGE = 'http://pygments.org/'
+HOMEPAGE = 'https://pygments.org/'
 
 # this refers to all files:
 LICENSE = '(same as %s)' % DISPLAY_NAME
